93fba20c528b50482b4caf0f9915864d1305fab2
DJIOTSA CHRISTIAN (el Mhulo) <djiotsadjouakechristiandaryn@gmail.com>
2024-08-24 11:56:22 +0100

    add nodemon module
    update package.js script

--- npm/intro.js
+++ npm/intro.js
@@ -15,9 +15,14 @@
 // npm init (step by step, press enter  to skip)
 // npm init -y (everything default)
 
+// npm i nodemon -D -- this module will watch our node code modification and restart the app. The -D/--save-dev define that nodemon is a devDependency
+// devDependencies are used for the development not in the production
+// after pushing the code to github(without node_modules folder) and cloning it, we just need to run 'npm install' to install all the dependencies
+
+// in the package.json you can write you own script -- eg: "dev" : "nodemon ./npm/intro.js". and run this using npm run dev
 
 const _ = require('lodash')
 
-const items = [1, 3,4,5,6, ["hello", 'how', 'aged', 456, 'are', 'you']]
+const items = [1,3,4,5,6,[90, 456]]
 const newItems = _.flattenDeep(items)
 console.log(newItems)
